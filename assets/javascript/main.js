98c6c34caeba32875d4052f1d6320c119b351652
Tina Nguyen <nguyentina2005@yahoo.com>
2017-06-07 01:04:11 -0400

    bugs

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -57,7 +57,7 @@ function getGif(){
 //clearing out the last gif so current gif could fill it up without the old ones there. 
   $("#theGIF").empty();
 //the url to get gif to seach for the anime that was  pick and put it only show 10 results
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userPick + "&api_key=dc6zaTOxFJmzC&limit=10";
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q="+userPick+"&api_key=dc6zaTOxFJmzC&limit=10";
 
   $.ajax({
       url: queryURL,
